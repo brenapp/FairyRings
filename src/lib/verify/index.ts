6ffa37f9490fb47559a3de0eeb9a6db2dc30a85f
MayorMonty <artificalchamber@gmail.com>
2019-04-03 01:38:57 -0400

    Verification System

--- src/lib/verify/index.ts
+++ src/lib/verify/index.ts
@@ -15,6 +15,10 @@ import {
 import { client } from "../../client";
 import { askString, choose } from "../prompt";
 
+const config: {
+  majors: { [college: string]: string[] };
+} = require("../../../config");
+
 export function findOrMakeRole(name: string, guild: Guild) {
   let role = guild.roles.find(role => role.name === name);
   return role
@@ -36,17 +40,43 @@ export default async function verify(member: GuildMember) {
     dm
   );
 
-  const college = await choose(
-    "Which college are you in? (Engineering, Science, Computing, Pre-Professional Health, Undeclared / Don't know)",
-    dm,
-    [
-      ["Engineering", "College of Engineering"],
-      ["Science", "College of Science"],
-      ["Computing", "College of Computing"],
-      ["Pre-Professional Health", "College of Pre-Professional Health"],
-      ["Undeclared", "Don't Know", "idk"]
-    ]
+  let college, major;
+
+  do {
+    college = await choose(
+      "Which college are you in? (Engineering, Science, Computing, Pre-Professional Health, Undeclared / Don't know)",
+      dm,
+      [
+        ...Object.keys(config.majors).map(a => [a, `College of ${a}`]),
+        ["Undeclared", "Don't Know", "idk"]
+      ]
+    );
+
+    if (college === "UNDECLARED") {
+      const confirmation = await choose("Confirm undeclared? (y/n)", dm, [
+        ["y", "yes", "yeah", "yep", "yeet"],
+        ["n", "no", "nope"]
+      ]);
+
+      major = confirmation === "Y" ? college : "BACK";
+    } else {
+      major = await choose(
+        `Thanks! Which of these is your major?\n\n${config.majors[college]
+          .map(m => `*${m}*`)
+          .join("\n")}\n\n You can say back to reselect your college`,
+        dm,
+        [...config.majors[college].map(a => [a]), ["back"]]
+      );
+    }
+  } while (major === "BACK");
+
+  const cuid = await askString("Finally, what's your CUID?", dm);
+
+  dm.send(
+    `Hi ${name}! You live in ${room} and you are studying ${major} in the College of ${college}`
   );
+
+  console.log("VERIFY", name, room, college, major, cuid);
 }
 
 client.on("guildMemberAdd", verify);
