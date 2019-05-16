1f5db4d926ecaa6c37846adef7b016b6fa0ebfbf
MayorMonty <artificalchamber@gmail.com>
2019-05-15 20:54:59 -0400

    Remove extra code

--- src/commands/debug.ts
+++ src/commands/debug.ts
@@ -4,7 +4,7 @@ import { client } from "../client";
 import * as vexdb from "vexdb";
 import keya from "keya";
 
-const MAYORMONTY = "274004148276690944";
+export const MAYORMONTY = "274004148276690944";
 
 function okay(message) {
   return message.author.id === MAYORMONTY;
@@ -30,30 +30,4 @@ addCommand("debug", (args, message) => {
   DEBUG = !DEBUG;
 
   message.channel.send(`Debug ${DEBUG ? "ENABLED" : "DISABLED"}`);
-});
-
-addCommand("cache", async (args, message) => {
-  if (!okay(message)) {
-    return false;
-  }
-
-  switch (args[0]) {
-    case "clear":
-      vexdb.cache.clear();
-      message.channel.send(`Cache Cleared`);
-      break;
-    case "":
-      let cache = Object.keys(await keya.all()).filter(key =>
-        key.includes("vexdb")
-      );
-      message.channel.send(["VexDB Current Cache", cache].join("\n"));
-  }
-});
-
-process.stderr.on("data", async chunk => {
-  if (DEBUG) {
-    let channel = await client.users.get(MAYORMONTY).createDM();
-
-    channel.send(chunk.toString());
-  }
 });
