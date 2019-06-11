db4b77cab5872b1d9e29a6c9f1643714ead3ff6e
MayorMonty <artificalchamber@gmail.com>
2019-06-11 02:39:48 -0400

    Don't import in report

--- src/behaviors/report.ts
+++ src/behaviors/report.ts
@@ -1,9 +1,8 @@
 import { Client, DiscordAPIError } from "discord.js";
-import { MAYORMONTY } from "../commands/debug";
 
 export default function report(client: Client) {
   return async (error: Error) => {
-    let me = await client.fetchUser(MAYORMONTY);
+    let me = await client.fetchUser("274004148276690944");
 
     me.send(error.stack);
   };
