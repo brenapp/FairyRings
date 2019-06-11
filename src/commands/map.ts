e2903c95f26271c189930e199f5c8f71a547072a
MayorMonty <artificalchamber@gmail.com>
2019-06-11 02:39:07 -0400

    Get rid of extra commands

--- src/commands/map.ts
+++
@@ -1,9 +0,0 @@
-import { addCommand } from "../lib/message";
-
-addCommand("map", async (args, message) => {
-  message.channel.send(
-    "https://www.clemson.edu/campus-map/pdfs/campus-map.pdf"
-  );
-
-  return true;
-});
--- src/commands/verify.ts
+++
@@ -1,7 +0,0 @@
-import { addCommand } from "../lib/message";
-import verify from "../behaviors/verify";
-
-addCommand("verify", (args, message) => {
-  message.mentions.members.forEach(member => verify(member));
-  return true;
-});
